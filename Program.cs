cb901323464385e598a1c9a0a4b98a2d4b1c0328
03Sly03 <slydevwebproject@outlook.fr>
2023-07-20 11:44:10 +0200

    ExerciceFigure

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using DemoInterface.Classes;
+using DemoInterface.Interfaces;
 
 Perche unePerche = new Perche();
 Baleine uneBaleine = new Baleine();
@@ -8,3 +9,44 @@ unePerche.Nager();
 uneBaleine.Nager();
 unExoset.Decoller();
 unExoset.Atterir();
+
+List<Animal> animaux = new List<Animal>()
+{
+    new Perche(),
+    new Perche(),
+    new Baleine(),
+    new Baleine(),
+    new Exoset(),
+    new Exoset(),
+    new ChauveSouris(),
+    new ChauveSouris(),
+    new Pigeon(),
+    new Pigeon(),
+};
+
+List<IPeutVoler> nageur = new List<IPeutVoler>()
+{
+    //new Perche(),
+    //new Perche(),
+    //new Baleine(),
+    //new Baleine(),
+    new Exoset(),
+    new Exoset(),
+    new ChauveSouris(),
+    new ChauveSouris(),
+    new Pigeon(),
+    new Pigeon(),
+};
+List<IPeutNager> volant = new List<IPeutNager>()
+{
+    new Perche(),
+    new Perche(),
+    new Baleine(),
+    new Baleine(),
+    new Exoset(),
+    new Exoset(),
+    //new ChauveSouris(),
+    //new ChauveSouris(),
+    //new Pigeon(),
+    //new Pigeon(),
+};
